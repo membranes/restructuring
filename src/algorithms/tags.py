3e73cb5fd70930cd1f5a909c0c90c6036e3d366d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-09 13:30:02 +0100

    Update src/algorithms/tags.py: In Progress

--- src/algorithms/tags.py
+++ src/algorithms/tags.py
@@ -1,17 +1,23 @@
-import os
+"""Module tags.py"""
+import collections
 import logging
+import os
 
+import dask.dataframe as dfr
 import pandas as pd
-
-import src.functions.streams
-import src.elements.text_attributes
 
 import config
 
 
 class Tags:
+    """
+    Class Tags
+    """
 
     def __init__(self):
+        """
+        Constructor
+        """
 
         self.__configurations = config.Config()
 
@@ -24,15 +30,31 @@ class Tags:
         self.__logger = logging.getLogger(__name__)
 
     def __data(self) -> pd.DataFrame:
+        """
 
-        text = src.elements.text_attributes.TextAttributes(uri=self.__uri, header=0)
-        streams = src.functions.streams.Streams()
+        :return:
+        """
 
-        return streams.read(text=text)
+        try:
+            data: dfr.DataFrame = dfr.read_csv(path=self.__uri, header=0)
+        except ImportError as err:
+            raise err from err
+
+        return data.compute()
 
     def exc(self):
+        """
 
+        :return:
+        """
+
+        # The data
         data = self.__data()
+
+        # The frequencies of the tags, by sentence and overarching
+        occurrences = data['tagstr'].str.split(pat=',', n=-1, expand=False).map(collections.Counter)
+        frequencies = data['tagstr'].str.split(pat=',', n=-1, expand=False).map(collections.Counter).sum()
+
         self.__logger.info(data.head())
-
-
+        self.__logger.info(occurrences)
+        self.__logger.info(frequencies)
