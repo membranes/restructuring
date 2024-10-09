fcdca02f1f54b20e40ec49ab8bf4d29dd337735b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-10-09 12:53:44 +0100

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -1,12 +1,26 @@
+"""Module interface.py"""
 import src.algorithms.networks
 
 
 class Interface:
+    """
+    The interface to the data package's classes
+    """
 
     def __init__(self):
+        """
+        Constructor
+        """
+
         self.__networks = src.algorithms.networks.Networks()
 
     def exc(self):
+        """
+
+        :return:
+        """
+
+        # Network Graphs
         networks = ['classes', 'details']
 
         for network in networks:
